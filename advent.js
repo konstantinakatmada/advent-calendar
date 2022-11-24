f8fc0cf220b855f64ae60f5cb5f78911313f84e3
Konstantina Katmada <konstantinakatmada@gmail.com>
2022-11-24 23:26:41 +0000

    images added

--- advent.js
+++ advent.js
@@ -2,9 +2,10 @@
 const calendarButton = document.querySelector(".btn-start");
 const calendarContainer = document.querySelector(".container");
 
+
 const calendarDays = 25;
 
-const openDoor = (path, event) => {
+const openDay = (path, event) => {
     event.target.parentNode.style.backgroundImage = `url(${path})`;
     event.target.style.opacity = "0";
     event.target.style.backgroundColor = "#521751";
@@ -12,23 +13,39 @@ const openDoor = (path, event) => {
 
 const createCalendar = () => {
     for (let i = 0; i < calendarDays; i++) {
-        const calendarDoor = document.createElement("div");
-        const calendarDoorText = document.createElement("div");
+        if (i === 15) {
+            //κώδικας για εισαγωγή βίντεο
+            const iframeElement = document.createElement('iframe');
+            iframeElement.width = "158";
+            iframeElement.height = "178";
+            iframeElement.src = "https://www.youtube.com/embed/npnp--SSx_8" 
+            iframeElement.title = "YouTube video player";
+            iframeElement.frameborder = "0";
+            iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            iframeElement.allowfullscreen= true;
+            iframeElement.classList.add('video');
+            calendarContainer.appendChild(iframeElement);
+        } else {
+            //κώδικας για εισαγωγή εικόνων
+            const calendarDay = document.createElement("div");
+            const calendarDayText = document.createElement("div");
+            calendarDay.classList.add("image");
+            calendarDay.style.gridArea = "day" + (i + 1);
+            calendarContainer.appendChild(calendarDay);
+            
 
-        calendarDoor.classList.add("image");
-        calendarDoor.style.gridArea = "day" + (i + 1);
-        calendarContainer.appendChild(calendarDoor);
+            calendarDayText.classList.add("text");
+            calendarDayText.innerHTML = i + 1;
+            calendarDay.appendChild(calendarDayText);
 
-        calendarDoorText.classList.add("text");
-        calendarDoorText.innerHTML = i + 1;
-        calendarDoor.appendChild(calendarDoorText);
+            imageNumber = i + 1;
+            let imagePath = `./images/image${imageNumber}.jpg`;
 
-        imageNumber = i + 1;
-        let imagePath = `./images/image${imageNumber}.jpg`;
-
-        calendarDoorText.addEventListener("click", openDoor.bind(null, imagePath));
+            calendarDayText.addEventListener("click", openDay.bind(null, imagePath));
+        }
     }
 }
+
 
 calendarButton.addEventListener("click", createCalendar);
 
