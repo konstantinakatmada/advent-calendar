d7dee6630d0b9abbe9d52b2a6a8e4fc141e35a4e
Konstantina Katmada <konstantinakatmada@gmail.com>
2022-11-26 01:33:05 +0000

    add more videos

--- advent.js
+++ advent.js
@@ -1,4 +1,4 @@
-//const myIframe= document.getElementById('myIframe');
+
 const calendarButton = document.querySelector(".btn-start");
 const calendarContainer = document.querySelector(".container");
 
@@ -14,32 +14,203 @@ const openDay = (path, event) => {
 const createCalendar = () => {
     for (let i = 0; i < calendarDays; i++) {
         if (i === 15) {
-            //κώδικας για εισαγωγή βίντεο
-            const iframeElement = document.createElement('iframe');
-            iframeElement.width = "148";
-            iframeElement.height = "168";
-            iframeElement.src = "https://www.youtube.com/embed/npnp--SSx_8";
-            iframeElement.title = "YouTube video player";
-            iframeElement.frameborder = "0";
-            iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            iframeElement.allowfullscreen= true;
-            iframeElement.classList.add('video');
-            calendarContainer.appendChild(iframeElement);
-        } else {
-            //κώδικας για εισαγωγή εικόνων
             const calendarDay = document.createElement("div");
             const calendarDayText = document.createElement("div");
             calendarDay.classList.add("image");
             calendarDay.style.gridArea = "day" + (i + 1);
             calendarContainer.appendChild(calendarDay);
-            
-
             calendarDayText.classList.add("text");
             calendarDayText.innerHTML = i + 1;
             calendarDay.appendChild(calendarDayText);
 
-            imageNumber = i + 1;
-            let imagePath = `./images/image${imageNumber}.jpg`;
+            const iframeElement = document.createElement('iframe');
+            iframeElement.width = "238";
+            iframeElement.height = "218";
+            iframeElement.src = "https://www.youtube.com/embed/G9d_L6JnkSQ";
+            iframeElement.title = "YouTube video player";
+            iframeElement.frameborder = "0";
+            iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+            iframeElement.allowfullscreen = true;
+            iframeElement.classList.add('video');
+
+
+
+            calendarDayText.addEventListener("click", (event) => {
+                event.target.parentNode.replaceChildren(iframeElement);
+            });
+        }
+           else if (i === 2) {
+                const calendarDay = document.createElement("div");
+                const calendarDayText = document.createElement("div");
+                calendarDay.classList.add("image");
+                calendarDay.style.gridArea = "day" + (i + 1);
+                calendarContainer.appendChild(calendarDay);
+                calendarDayText.classList.add("text");
+                calendarDayText.innerHTML = i + 1;
+                calendarDay.appendChild(calendarDayText);
+    
+                const iframeElement = document.createElement('iframe');
+                iframeElement.width = "238";
+                iframeElement.height = "218";
+                iframeElement.src = 'https://www.youtube.com/embed/-y6t1N8qTIU';
+                iframeElement.title = "YouTube video player";
+                iframeElement.frameborder = "0";
+                iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+                iframeElement.allowfullscreen = true;
+                iframeElement.classList.add('video');
+    
+    
+    
+                calendarDayText.addEventListener("click", (event) => {
+                    event.target.parentNode.replaceChildren(iframeElement);
+                });
+            }
+        else if (i === 6) {
+            const calendarDay = document.createElement("div");
+            const calendarDayText = document.createElement("div");
+            calendarDay.classList.add("image");
+            calendarDay.style.gridArea = "day" + (i + 1);
+            calendarContainer.appendChild(calendarDay);
+            calendarDayText.classList.add("text");
+            calendarDayText.innerHTML = i + 1;
+            calendarDay.appendChild(calendarDayText);
+
+            const iframeElement = document.createElement('iframe');
+            iframeElement.width = "238";
+            iframeElement.height = "218";
+            iframeElement.src = "https://www.youtube.com/embed/jWRbbHe1ryU";
+            iframeElement.title = "YouTube video player";
+            iframeElement.frameborder = "0";
+            iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+            iframeElement.allowfullscreen = true;
+            iframeElement.classList.add('video');
+
+
+
+            calendarDayText.addEventListener("click", (event) => {
+                event.target.parentNode.replaceChildren(iframeElement);
+            });
+
+        } 
+        else if (i === 11) {
+            const calendarDay = document.createElement("div");
+            const calendarDayText = document.createElement("div");
+            calendarDay.classList.add("image");
+            calendarDay.style.gridArea = "day" + (i + 1);
+            calendarContainer.appendChild(calendarDay);
+            calendarDayText.classList.add("text");
+            calendarDayText.innerHTML = i + 1;
+            calendarDay.appendChild(calendarDayText);
+
+            const iframeElement = document.createElement('iframe');
+            iframeElement.width = "238";
+            iframeElement.height = "218";
+            iframeElement.src = "https://www.youtube.com/embed/H2ifOP25wLs";
+            iframeElement.title = "YouTube video player";
+            iframeElement.frameborder = "0";
+            iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+            iframeElement.allowfullscreen = true;
+            iframeElement.classList.add('video');
+
+
+
+            calendarDayText.addEventListener("click", (event) => {
+                event.target.parentNode.replaceChildren(iframeElement);
+            });
+
+        } 
+        else if (i === 13) {
+            const calendarDay = document.createElement("div");
+            const calendarDayText = document.createElement("div");
+            calendarDay.classList.add("image");
+            calendarDay.style.gridArea = "day" + (i + 1);
+            calendarContainer.appendChild(calendarDay);
+            calendarDayText.classList.add("text");
+            calendarDayText.innerHTML = i + 1;
+            calendarDay.appendChild(calendarDayText);
+
+            const iframeElement = document.createElement('iframe');
+            iframeElement.width = "238";
+            iframeElement.height = "218";
+            iframeElement.src = "https://www.youtube.com/embed/ZtjKa72uq9k";
+            iframeElement.title = "YouTube video player";
+            iframeElement.frameborder = "0";
+            iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+            iframeElement.allowfullscreen = true;
+            iframeElement.classList.add('video');
+
+
+
+            calendarDayText.addEventListener("click", (event) => {
+                event.target.parentNode.replaceChildren(iframeElement);
+            });
+
+        } 
+        else if (i === 21) {
+            const calendarDay = document.createElement("div");
+            const calendarDayText = document.createElement("div");
+            calendarDay.classList.add("image");
+            calendarDay.style.gridArea = "day" + (i + 1);
+            calendarContainer.appendChild(calendarDay);
+            calendarDayText.classList.add("text");
+            calendarDayText.innerHTML = i + 1;
+            calendarDay.appendChild(calendarDayText);
+
+            const iframeElement = document.createElement('iframe');
+            iframeElement.width = "238";
+            iframeElement.height = "218";
+            iframeElement.src = "https://www.youtube.com/embed/Oq3gi9IUkws";
+            iframeElement.title = "YouTube video player";
+            iframeElement.frameborder = "0";
+            iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+            iframeElement.allowfullscreen = true;
+            iframeElement.classList.add('video');
+
+
+
+            calendarDayText.addEventListener("click", (event) => {
+                event.target.parentNode.replaceChildren(iframeElement);
+            });
+        }
+        else if (i === 24) {
+            const calendarDay = document.createElement("div");
+            const calendarDayText = document.createElement("div");
+            calendarDay.classList.add("image");
+            calendarDay.style.gridArea = "day" + (i + 1);
+            calendarContainer.appendChild(calendarDay);
+            calendarDayText.classList.add("text");
+            calendarDayText.innerHTML = i + 1;
+            calendarDay.appendChild(calendarDayText);
+
+            const iframeElement = document.createElement('iframe');
+            iframeElement.width = "238";
+            iframeElement.height = "218";
+            iframeElement.src = "https://www.youtube.com/embed/f2UnLAU1x_I";
+            iframeElement.title = "YouTube video player";
+            iframeElement.frameborder = "0";
+            iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+            iframeElement.allowfullscreen = true;
+            iframeElement.classList.add('video');
+
+
+
+            calendarDayText.addEventListener("click", (event) => {
+                event.target.parentNode.replaceChildren(iframeElement);
+            });
+
+        } else {
+
+            const calendarDay = document.createElement("div");
+            const calendarDayText = document.createElement("div");
+            calendarDay.classList.add("image");
+            calendarDay.style.gridArea = "day" + (i + 1);
+            calendarContainer.appendChild(calendarDay);
+            calendarDayText.classList.add("text");
+            calendarDayText.innerHTML = i + 1;
+            calendarDay.appendChild(calendarDayText);
+
+            const imageNumber = i + 1;
+            const imagePath = `./images/image${imageNumber}.jpg`;
 
             calendarDayText.addEventListener("click", openDay.bind(null, imagePath));
         }
