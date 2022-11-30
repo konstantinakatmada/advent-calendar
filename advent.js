df2673e7624f1dee4063abd487f36ff3bd8815e4
Konstantina Katmada <konstantinakatmada@gmail.com>
2022-11-30 00:15:06 +0000

    days refactored formatted file

--- advent.js
+++ advent.js
@@ -1,264 +1,65 @@
-
 const calendarButton = document.querySelector(".btn-start");
 const calendarContainer = document.querySelector(".container");
-
-
 const calendarDays = 25;
 
-const openDay = (path, event) => {
-    event.target.parentNode.style.backgroundImage = `url(${path})`;
-    event.target.style.opacity = "0";
-    event.target.style.backgroundColor = "#521751";
+const doorsOpened = []
+
+const setVideo = (i, youtubeURL, isAfterToday) => {
+    const calendarDay = document.createElement("div");
+    const calendarDayText = document.createElement("div");
+    calendarDay.classList.add("image");
+    calendarDay.style.gridArea = "day" + (i + 1);
+    calendarContainer.appendChild(calendarDay);
+    calendarDayText.classList.add("text");
+    calendarDayText.innerHTML = i + 1;
+    calendarDay.appendChild(calendarDayText);
+    const iframeElement = document.createElement('iframe');
+    iframeElement.width = "238";
+    iframeElement.height = "218";
+    iframeElement.src = youtubeURL;
+    iframeElement.title = "YouTube video player";
+    iframeElement.frameborder = "0";
+    iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+    iframeElement.allowfullscreen = true;
+    iframeElement.classList.add('video');
+    if (!isAfterToday) {
+        calendarDayText.addEventListener("click", (event) => {
+            doorsOpened.push(i)
+            localStorage.setItem('doorsOpened', JSON.stringify(doorsOpened))
+            event.target.parentNode.replaceChildren(iframeElement);
+        });
+    }
 }
 
 const createCalendar = () => {
+    const todayDate = document.getElementById('start').value;
     for (let i = 0; i < calendarDays; i++) {
+        const date = `2022-12-${String(i + 1).padStart(2, '0')}`
+        const isAfterToday = todayDate < date
         if (i === 15) {
-            const todayDate = document.getElementById('start').value;
-            if (todayDate < '2022-12-16') {
-                continue;
-            }
-            const calendarDay = document.createElement("div");
-            const calendarDayText = document.createElement("div");
-            calendarDay.classList.add("image");
-            calendarDay.style.gridArea = "day" + (i + 1);
-            calendarContainer.appendChild(calendarDay);
-            calendarDayText.classList.add("text");
-            calendarDayText.innerHTML = i + 1;
-            calendarDay.appendChild(calendarDayText);
-
-            const iframeElement = document.createElement('iframe');
-            iframeElement.width = "238";
-            iframeElement.height = "218";
-            iframeElement.src = "https://www.youtube.com/embed/G9d_L6JnkSQ";
-            iframeElement.title = "YouTube video player";
-            iframeElement.frameborder = "0";
-            iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-            iframeElement.allowfullscreen = true;
-            iframeElement.classList.add('video');
-
-
-
-            calendarDayText.addEventListener("click", (event) => {
-                event.target.parentNode.replaceChildren(iframeElement);
-            });
+            setVideo(i, 'https://www.youtube.com/embed/G9d_L6JnkSQ', isAfterToday)
         }
         else if (i === 2) {
-            const todayDate = document.getElementById('start').value;
-            if (todayDate < '2022-12-03') {
-                continue;
-            }
-            const calendarDay = document.createElement("div");
-            const calendarDayText = document.createElement("div");
-            calendarDay.classList.add("image");
-            calendarDay.style.gridArea = "day" + (i + 1);
-            calendarContainer.appendChild(calendarDay);
-            calendarDayText.classList.add("text");
-            calendarDayText.innerHTML = i + 1;
-            calendarDay.appendChild(calendarDayText);
-
-            const iframeElement = document.createElement('iframe');
-            iframeElement.width = "238";
-            iframeElement.height = "218";
-            iframeElement.src = 'https://www.youtube.com/embed/-y6t1N8qTIU';
-            iframeElement.title = "YouTube video player";
-            iframeElement.frameborder = "0";
-            iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-            iframeElement.allowfullscreen = true;
-            iframeElement.classList.add('video');
-
-
-
-            calendarDayText.addEventListener("click", (event) => {
-                event.target.parentNode.replaceChildren(iframeElement);
-            });
+            setVideo(i, 'https://www.youtube.com/embed/-y6t1N8qTIU', isAfterToday)
         }
         else if (i === 6) {
-            const todayDate = document.getElementById('start').value;
-            if (todayDate < '2022-12-07') {
-                continue;
-            }
-            const calendarDay = document.createElement("div");
-            const calendarDayText = document.createElement("div");
-            calendarDay.classList.add("image");
-            calendarDay.style.gridArea = "day" + (i + 1);
-            calendarContainer.appendChild(calendarDay);
-            calendarDayText.classList.add("text");
-            calendarDayText.innerHTML = i + 1;
-            calendarDay.appendChild(calendarDayText);
-
-            const iframeElement = document.createElement('iframe');
-            iframeElement.width = "238";
-            iframeElement.height = "218";
-            iframeElement.src = "https://www.youtube.com/embed/jWRbbHe1ryU";
-            iframeElement.title = "YouTube video player";
-            iframeElement.frameborder = "0";
-            iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-            iframeElement.allowfullscreen = true;
-            iframeElement.classList.add('video');
-
-
-
-            calendarDayText.addEventListener("click", (event) => {
-                event.target.parentNode.replaceChildren(iframeElement);
-            });
-
+            setVideo(i, "https://www.youtube.com/embed/jWRbbHe1ryU", isAfterToday)
         }
         else if (i === 8) {
-            const todayDate = document.getElementById('start').value;
-            if (todayDate < '2022-12-09') {
-                continue;
-            }
-
-            const calendarDay = document.createElement("div");
-            const calendarDayText = document.createElement("div");
-            calendarDay.classList.add("image");
-            calendarDay.style.gridArea = "day" + (i + 1);
-            calendarContainer.appendChild(calendarDay);
-            calendarDayText.classList.add("text");
-            calendarDayText.innerHTML = i + 1;
-            calendarDay.appendChild(calendarDayText);
-
-            const iframeElement = document.createElement('iframe');
-            iframeElement.width = "238";
-            iframeElement.height = "218";
-            iframeElement.src = 'https://www.youtube.com/embed/f2UnLAU1x_I';
-            iframeElement.title = "YouTube video player";
-            iframeElement.frameborder = "0";
-            iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-            iframeElement.allowfullscreen = true;
-            iframeElement.classList.add('video');
-
-
-
-            calendarDayText.addEventListener("click", (event) => {
-                event.target.parentNode.replaceChildren(iframeElement);
-            });
+            setVideo(i, "https://www.youtube.com/embed/f2UnLAU1x_I", isAfterToday)
         }
         else if (i === 11) {
-            const todayDate = document.getElementById('start').value;
-            if (todayDate < '2022-12-12') {
-                continue;
-            }
-            const calendarDay = document.createElement("div");
-            const calendarDayText = document.createElement("div");
-            calendarDay.classList.add("image");
-            calendarDay.style.gridArea = "day" + (i + 1);
-            calendarContainer.appendChild(calendarDay);
-            calendarDayText.classList.add("text");
-            calendarDayText.innerHTML = i + 1;
-            calendarDay.appendChild(calendarDayText);
-
-            const iframeElement = document.createElement('iframe');
-            iframeElement.width = "238";
-            iframeElement.height = "218";
-            iframeElement.src = "https://www.youtube.com/embed/H2ifOP25wLs";
-            iframeElement.title = "YouTube video player";
-            iframeElement.frameborder = "0";
-            iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-            iframeElement.allowfullscreen = true;
-            iframeElement.classList.add('video');
-
-
-
-            calendarDayText.addEventListener("click", (event) => {
-                event.target.parentNode.replaceChildren(iframeElement);
-            });
-
+            setVideo(i, "https://www.youtube.com/embed/H2ifOP25wLs", isAfterToday)
         }
         else if (i === 13) {
-            const todayDate = document.getElementById('start').value;
-            if (todayDate < '2022-12-14') {
-                continue;
-            }
-            const calendarDay = document.createElement("div");
-            const calendarDayText = document.createElement("div");
-            calendarDay.classList.add("image");
-            calendarDay.style.gridArea = "day" + (i + 1);
-            calendarContainer.appendChild(calendarDay);
-            calendarDayText.classList.add("text");
-            calendarDayText.innerHTML = i + 1;
-            calendarDay.appendChild(calendarDayText);
-
-            const iframeElement = document.createElement('iframe');
-            iframeElement.width = "238";
-            iframeElement.height = "218";
-            iframeElement.src = "https://www.youtube.com/embed/ZtjKa72uq9k";
-            iframeElement.title = "YouTube video player";
-            iframeElement.frameborder = "0";
-            iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-            iframeElement.allowfullscreen = true;
-            iframeElement.classList.add('video');
-
-
-
-            calendarDayText.addEventListener("click", (event) => {
-                event.target.parentNode.replaceChildren(iframeElement);
-            });
-
+            setVideo(i, "https://www.youtube.com/embed/ZtjKa72uq9k", isAfterToday)
         }
         else if (i === 21) {
-            const todayDate = document.getElementById('start').value;
-            if (todayDate < '2022-12-22') {
-                continue;
-            }
-            const calendarDay = document.createElement("div");
-            const calendarDayText = document.createElement("div");
-            calendarDay.classList.add("image");
-            calendarDay.style.gridArea = "day" + (i + 1);
-            calendarContainer.appendChild(calendarDay);
-            calendarDayText.classList.add("text");
-            calendarDayText.innerHTML = i + 1;
-            calendarDay.appendChild(calendarDayText);
-
-            const iframeElement = document.createElement('iframe');
-            iframeElement.width = "238";
-            iframeElement.height = "218";
-            iframeElement.src = "https://www.youtube.com/embed/Oq3gi9IUkws";
-            iframeElement.title = "YouTube video player";
-            iframeElement.frameborder = "0";
-            iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-            iframeElement.allowfullscreen = true;
-            iframeElement.classList.add('video');
-
-
-
-            calendarDayText.addEventListener("click", (event) => {
-                event.target.parentNode.replaceChildren(iframeElement);
-            });
+            setVideo(i, "https://www.youtube.com/embed/Oq3gi9IUkws", isAfterToday)
         }
         else if (i === 24) {
-            const todayDate = document.getElementById('start').value;
-            if (todayDate < '2022-12-25') {
-                continue;
-            }
-            const calendarDay = document.createElement("div");
-            const calendarDayText = document.createElement("div");
-            calendarDay.classList.add("image");
-            calendarDay.style.gridArea = "day" + (i + 1);
-            calendarContainer.appendChild(calendarDay);
-            calendarDayText.classList.add("text");
-            calendarDayText.innerHTML = i + 1;
-            calendarDay.appendChild(calendarDayText);
-
-            const iframeElement = document.createElement('iframe');
-            iframeElement.width = "238";
-            iframeElement.height = "218";
-            iframeElement.src = "https://www.youtube.com/embed/feYR2StoV3s";
-            iframeElement.title = "YouTube video player";
-            iframeElement.frameborder = "0";
-            iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-            iframeElement.allowfullscreen = true;
-            iframeElement.classList.add('video');
-
-
-
-            calendarDayText.addEventListener("click", (event) => {
-                event.target.parentNode.replaceChildren(iframeElement);
-            });
-
+            setVideo(i, "https://www.youtube.com/embed/feYR2StoV3s", isAfterToday)
         } else {
-
             const calendarDay = document.createElement("div");
             const calendarDayText = document.createElement("div");
             calendarDay.classList.add("image");
@@ -267,15 +68,20 @@ const createCalendar = () => {
             calendarDayText.classList.add("text");
             calendarDayText.innerHTML = i + 1;
             calendarDay.appendChild(calendarDayText);
-
             const imageNumber = i + 1;
             const imagePath = `./images/image${imageNumber}.jpg`;
-
-            calendarDayText.addEventListener("click", openDay.bind(null, imagePath));
+            if (!isAfterToday) {
+                calendarDayText.addEventListener("click", (event) => {
+                    doorsOpened.push(i)
+                    localStorage.setItem('doorsOpened', JSON.stringify(doorsOpened))
+                    event.target.parentNode.style.backgroundImage = `url(${imagePath})`;
+                    event.target.style.opacity = "0";
+                    event.target.style.backgroundColor = "#521751";
+                }
+                );
+            }
         }
     }
 }
 
-
 calendarButton.addEventListener("click", createCalendar);
-
